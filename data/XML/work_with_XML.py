ce749ed8813dfb28fe125762735798c68382ada2
Sergey Polozyuk <Sergey>
2021-03-27 14:28:34 +0200

    First alfa-version programm `1 day version 1

--- data/XML/work_with_XML.py
+++ data/XML/work_with_XML.py
@@ -1,10 +1,17 @@
 import xml.etree.ElementTree as ET
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-root_conf = ET.parse(os.path.join(ROOT_DIR,'conf.xml')).getroot()
-root_osnova = ET.parse(os.path.join(ROOT_DIR,'osnova.xml')).getroot()
+
 def get_attr_XML(name):
+    root_conf = ET.parse(os.path.join(ROOT_DIR, 'conf.xml')).getroot()
     return root_conf.find(name).text
 
 def get_osnova_XML(name):
+    root_osnova = ET.parse(os.path.join(ROOT_DIR, 'osnova.xml')).getroot()
     return root_osnova.find(name).text
+
+def change_val_XML(name_file_XML, val,new_val):
+     root_change = ET.parse(os.path.join(ROOT_DIR, name_file_XML +'.xml'))
+     for t in root_change.iterfind(val):
+         t.text = new_val
+     root_change.write(os.path.join(ROOT_DIR, name_file_XML +'.xml'),encoding="UTF-8",xml_declaration=True)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
 import os.path
+import os
 from PyQt5 import QtWebEngineWidgets
 class Window(QMainWindow):
     def __init__(self):
@@ -42,9 +43,14 @@ class Window(QMainWindow):
         saveAsAction.setShortcut('Alt+S')
         saveAsAction.triggered.connect(self.save_file_as)
 
+        RunAction = QAction('Run', self)
+        RunAction.setShortcut('Ctrl+R')
+        RunAction.triggered.connect(self.run_compile)
+
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(newAction)
+        fileMenu.addAction(RunAction)
         fileMenu.addAction(openAction)
         fileMenu.addAction(saveAction)
         fileMenu.addAction(saveAsAction)
@@ -55,16 +61,20 @@ class Window(QMainWindow):
         form_frame.setFrameShape(QFrame.StyledPanel)
         form_lay = QFormLayout()
         form_lay.addRow(self.f_label)
+        if XML.get_osnova_XML('tec-address') != "" and XML.get_osnova_XML('tec-name-file') != "":
+            with open(XML.get_osnova_XML('tec-address') +"/" + XML.get_osnova_XML('tec-name-file') + ".tex", encoding='utf-8') as f:
+                self.f_label.setPlainText(f.read())
+
         form_frame.setLayout(form_lay)
 
         ver_frame = QFrame()
         ver_frame.setFrameShape(QFrame.StyledPanel)
 
-        v = QWebEngineView()
-        v.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
-        v.load(QUrl("file:///alife.pdf"))
+        self.v = QWebEngineView()
+        self.v.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
+        self.v.load(QUrl("file:///"+XML.get_osnova_XML('tec-address') +"/" + XML.get_osnova_XML('tec-name-file') + ".pdf"))
         ver_box = QVBoxLayout()
-        ver_box.addWidget(v)
+        ver_box.addWidget(self.v)
         ver_box.setContentsMargins(10, 10, 10, 10)
         ver_frame.setLayout(ver_box)
 
@@ -77,29 +87,41 @@ class Window(QMainWindow):
         self.setCentralWidget(splitter)
 
     def save_file(self):
-        with open(XML.get_osnova_XML('tec-address') + XML.get_osnova_XML('tec-name-file') + ".tex", 'w', encoding='utf-8') as f:
+        with open(XML.get_osnova_XML('tec-address') +"/" + XML.get_osnova_XML('tec-name-file') + ".tex", 'w', encoding='utf-8') as f:
             my_text = self.f_label.toPlainText()
             f.write(my_text)
 
     def save_file_as(self):
-        file = QtWidgets.QFileDialog.getSaveFileName(self,"Save file as","./","Tex files (*.tex)")
-        if not file:
-             return
+        file = QtWidgets.QFileDialog.getSaveFileName(self,"Save file as",XML.get_osnova_XML('tec-address'),"Tex files (*.tex)")
+        print(file)#Взять название нового файла.
+        if file[0] == "":
+            return
         else:
+            XML.change_val_XML('osnova', 'tec-address', os.path.split(file[0])[0])
+            XML.change_val_XML('osnova', 'tec-name-file', os.path.splitext(os.path.basename(file[0]))[0])
             with open(file[0], 'w', encoding='utf-8') as f:
                 my_text = self.f_label.toPlainText()
                 f.write(my_text)
 
     def open_file(self):
-        file, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"Open file","./","Tex files (*.tex)")
+        file, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"Open file",XML.get_osnova_XML('tec-address'),"Tex files (*.tex)")
         if not file:
              return
         else:
+            XML.change_val_XML('osnova', 'tec-address', os.path.split(file[0])[0])
+            XML.change_val_XML('osnova', 'tec-name-file', os.path.splitext(os.path.basename(file[0]))[0])
             with open(file[0], encoding='utf-8') as f:
                 self.f_label.setPlainText(f.read())
 
     def new_file(self):
+
         self.f_label.setPlainText("")
+
+    def run_compile(self):
+        print("cmd /c pdflatex " + XML.get_osnova_XML('tec-name-file') + ".tex")
+        os.chdir(XML.get_osnova_XML('tec-address') + '/')
+        os.system("cmd /c pdflatex " + XML.get_osnova_XML('tec-name-file') + ".tex" )
+        self.v.reload()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
